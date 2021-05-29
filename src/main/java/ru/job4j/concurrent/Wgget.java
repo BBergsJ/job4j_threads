ff680ae1ddff5cd93fa00e89aff5505b7956af7f
BBergsJ <goldlike888@gmail.com>
2021-05-29 22:53:42 +0300

    Скачивание файла с ограничением

---
+++ src/main/java/ru/job4j/concurrent/Wgget.java
@@ -0,0 +1,24 @@
+package ru.job4j.concurrent;
+
+public class Wgget implements Runnable {
+    private final String url;
+    private final int speed;
+
+    public Wgget(String url, int speed) {
+        this.url = url;
+        this.speed = speed;
+    }
+
+    @Override
+    public void run() {
+        /* Скачать файл*/
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        String url = args[0];
+        int speed = Integer.parseInt(args[1]);
+        Thread wget = new Thread(new Wgget(url, speed));
+        wget.start();
+        wget.join();
+    }
+}
