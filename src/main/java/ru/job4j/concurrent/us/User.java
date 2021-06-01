2c8f0ddcee2e491d05bba46e8e202a3b45285c98
BBergsJ <goldlike888@gmail.com>
2021-06-01 23:24:05 +0300

    Класс хранилища пользователей UserStorage

---
+++ src/main/java/ru/job4j/concurrent/us/User.java
@@ -0,0 +1,25 @@
+package ru.job4j.concurrent.us;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+@ThreadSafe
+public final class User {
+    @GuardedBy("this")
+    private final int id;
+    @GuardedBy("this")
+    private final int amount;
+
+    public User(int id, int amount) {
+        this.id = id;
+        this.amount = amount;
+    }
+
+    public synchronized int getId() {
+        return id;
+    }
+
+    public synchronized int getAmount() {
+        return amount;
+    }
+}
---
+++ src/main/java/ru/job4j/concurrent/us/UserStorage.java
@@ -0,0 +1,55 @@
+package ru.job4j.concurrent.us;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@ThreadSafe
+public class UserStorage {
+
+    private final List<User> users = new ArrayList<>();
+
+    public boolean add(User user) {
+        for (User u : users) {
+            if (user.getId() != u.getId()) {
+                users.add(user);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean update(User user) {
+        for (User u : users) {
+            if (user.getId() == u.getId()) {
+                users.set(u.getId() - 1, user);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean delete(User user) {
+        for (User u : users) {
+            if (user.getId() == u.getId()) {
+                users.remove(u);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean transfer(int fromId, int toId, int amount) {
+        return false;
+    }
+
+    public static void main(String[] args) {
+        UserStorage storage = new UserStorage();
+        storage.add(new User(1, 100));
+        storage.add(new User(2, 200));
+
+        storage.transfer(1, 2, 50);
+    }
+}
