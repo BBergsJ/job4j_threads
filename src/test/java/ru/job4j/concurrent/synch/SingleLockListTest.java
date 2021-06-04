9f9f6f571ff7ea925004e4ed66c5320ae26f6266
BBergsJ <goldlike888@gmail.com>
2021-06-04 20:34:57 +0300

    ThreadSafe динамический список

--- src/test/java/ru/job4j/concurrent/synch/SingleLockListTest.java
+++ src/test/java/ru/job4j/concurrent/synch/SingleLockListTest.java
@@ -2,9 +2,8 @@ package ru.job4j.concurrent.synch;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
@@ -23,5 +22,42 @@ public class SingleLockListTest {
         Set<Integer> rsl = new TreeSet<>();
         list.iterator().forEachRemaining(rsl::add);
         assertThat(rsl, is(Set.of(1, 2)));
+    }
+
+    @Test
+    public void get() throws InterruptedException {
+        SingleLockList<Integer> list = new SingleLockList<>(new ArrayList<>());
+        AtomicInteger intRslFirst = new AtomicInteger();
+        AtomicInteger intRslSecond = new AtomicInteger();
+        Thread first = new Thread(() -> {
+                list.add(1);
+                intRslFirst.set(list.get(0));
+            }
+        );
+        Thread second = new Thread(() -> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+                list.add(2);
+                intRslSecond.set(list.get(1));
+            }
+        );
+        first.start();
+        second.start();
+        first.join();
+        second.join();
+        assertThat(intRslFirst.get(), is(1));
+        assertThat(intRslSecond.get(), is(2));
+    }
+
+    @Test
+    public void linkedToList() {
+        SingleLockList<Integer> linked = new SingleLockList<Integer>(new LinkedList<>());
+        linked.add(1);
+        List<Integer> expected = new LinkedList<>();
+        linked.iterator().forEachRemaining(expected::add);
+        assertThat(expected, is(List.of(1)));
     }
 }
