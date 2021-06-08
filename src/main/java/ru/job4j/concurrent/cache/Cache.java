2480006199cf8bd5b82a4dbd73d89e7e0a8fe314
BBergsJ <goldlike888@gmail.com>
2021-06-08 21:27:05 +0300

    Неблокирующий кеш

---
+++ src/main/java/ru/job4j/concurrent/cache/Cache.java
@@ -0,0 +1,22 @@
+package ru.job4j.concurrent.cache;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Cache {
+    private final Map<Integer, Base> memory = new ConcurrentHashMap<>();
+
+    public boolean add(Base model) {
+        /* TODO impl */
+        return false;
+    }
+
+    public boolean update(Base model) {
+        /* TODO impl */
+        return false;
+    }
+
+    public void delete(Base model) {
+        /* TODO impl */
+    }
+}
