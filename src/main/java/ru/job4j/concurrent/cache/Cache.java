b07d05d8e9fb3a34e39a7ee92a563e9eacfb4c35
BBergsJ <goldlike888@gmail.com>
2021-06-08 22:01:39 +0300

    Неблокирующий кеш

--- src/main/java/ru/job4j/concurrent/cache/Cache.java
+++ src/main/java/ru/job4j/concurrent/cache/Cache.java
@@ -2,6 +2,7 @@ package ru.job4j.concurrent.cache;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.BiFunction;
 
 public class Cache {
     private final Map<Integer, Base> memory = new ConcurrentHashMap<>();
@@ -11,10 +12,22 @@ public class Cache {
     }
 
     public boolean update(Base model) {
-        return memory.replace(model.getId(), model) != null;
+        Base stored = memory.get(model.getId());
+        if (stored.getVersion() != model.getVersion()) {
+            throw new OptimisticException("Versions are not equal");
+        }
+        return true;
     }
 
     public void delete(Base model) {
         memory.remove(model.getId(), model);
+    }
+
+    public static void main(String[] args) {
+        Base base1 = new Base(1, 1);
+        Base base2 = new Base(2, 1);
+
+        Cache cache = new Cache();
+        BiFunction biFunction;
     }
 }
