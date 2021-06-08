e661ed9f390da89f27cb45ce9803d7fd4c1f19d9
Dmitry <goldlike888@gmail.com>
2021-06-08 12:47:31 +0300

    CAS - операции

--- src/main/java/ru/job4j/concurrent/cas/CASCount.java
+++ src/main/java/ru/job4j/concurrent/cas/CASCount.java
@@ -9,22 +9,40 @@ public class CASCount {
     private AtomicReference<Integer> count = new AtomicReference<>();
 
     public void increment() {
-        int ref;
-        int refInc;
+        Integer temp;
         do {
-            ref = count.get();
-            refInc = ref++;
-        } while (count.compareAndSet(ref, refInc));
+            temp = count.get();
+        } while (!count.compareAndSet(temp, temp + 1));
     }
 
     public int get() {
         return count.get();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         CASCount casCount = new CASCount();
-        casCount.count.set(10);
-        casCount.increment();
+        casCount.count.set(0);
+
+         Thread thread1 = new Thread(() -> {
+             while (!Thread.currentThread().isInterrupted()) {
+                 casCount.increment();
+             }
+         });
+
+         Thread thread2 = new Thread(() -> {
+             while (!Thread.currentThread().isInterrupted()) {
+                 casCount.increment();
+             }
+         });
+
+         thread1.start();
+         thread2.start();
+         Thread.sleep(1000);
+         thread1.interrupt();
+         thread2.interrupt();
+         thread1.join();
+         thread2.join();
+
         System.out.println(casCount.get());
     }
 }
