9504a17b3641193595350053c3936b69da2b4211
Dmitry <goldlike888@gmail.com>
2021-06-09 11:54:03 +0300

    Неблокирующий кеш

--- src/main/java/ru/job4j/concurrent/cache/Cache.java
+++ src/main/java/ru/job4j/concurrent/cache/Cache.java
@@ -24,36 +24,4 @@ public class Cache {
     public boolean delete(Base model) {
         return memory.remove(model.getId(), model);
     }
-
-    public static void main(String[] args) {
-        Base base1 = new Base(1, 1);
-        Base base2 = new Base(2, 1);
-        Base base3 = new Base(3, 1);
-
-        Cache cache = new Cache();
-        cache.add(base1);
-        cache.add(base2);
-        cache.add(base3);
-
-        for (Map.Entry<Integer, Base> integerBaseEntry : cache.memory.entrySet()) {
-            System.out.println(integerBaseEntry);
-        }
-
-        System.out.println(System.lineSeparator());
-
-        cache.update(cache.memory.get(base1.getId()));
-
-
-        for (Map.Entry<Integer, Base> integerBaseEntry : cache.memory.entrySet()) {
-            System.out.println(integerBaseEntry);
-        }
-
-        System.out.println(System.lineSeparator());
-
-        cache.update(cache.memory.get(base1.getId()));
-
-        for (Map.Entry<Integer, Base> integerBaseEntry : cache.memory.entrySet()) {
-            System.out.println(integerBaseEntry);
-        }
-    }
 }
