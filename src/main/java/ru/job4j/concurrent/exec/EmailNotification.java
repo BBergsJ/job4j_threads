83a78feab666c0cead827e8d82e2341fcb159559
Dmitry <goldlike888@gmail.com>
2021-06-11 16:40:04 +0300

    ExecutorService рассылка почты

--- src/main/java/ru/job4j/concurrent/exec/EmailNotification.java
+++ src/main/java/ru/job4j/concurrent/exec/EmailNotification.java
@@ -57,9 +57,9 @@ public class EmailNotification {
 
     public void send(String subject, String body, String email) {
         System.out.println(
-                "subject = " + subject + System.lineSeparator() +
-                "body = " + body +  System.lineSeparator() +
-                "email : " + email + System.lineSeparator()
+                "subject = " + subject + System.lineSeparator()
+                        + "body = " + body +  System.lineSeparator()
+                        + "email : " + email + System.lineSeparator()
         );
     }
 
