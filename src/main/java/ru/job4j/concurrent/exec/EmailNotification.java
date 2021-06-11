25f18d639dcefb1f214712e57700d0b62cb50ee6
Dmitry <goldlike888@gmail.com>
2021-06-11 12:50:39 +0300

    ExecutorService рассылка почты

--- src/main/java/ru/job4j/concurrent/exec/EmailNotification.java
+++ src/main/java/ru/job4j/concurrent/exec/EmailNotification.java
@@ -1,4 +1,40 @@
 package ru.job4j.concurrent.exec;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class EmailNotification {
+
+    private final ExecutorService pool;
+
+    public EmailNotification() {
+        this.pool = Executors.newFixedThreadPool(
+                Runtime.getRuntime().availableProcessors());
+    }
+
+    public void emailTo(User user) {
+        pool.submit(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Execute " + Thread.currentThread().getName());
+            }
+        });
+    }
+
+    public void close(){
+        pool.shutdown();
+        while (!pool.isTerminated()) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                Thread.currentThread().interrupt();
+            }
+        }
+        System.out.println("Execute " + Thread.currentThread().getName());
+    }
+
+    public void send(String subject, String body, String email) {
+
+    }
 }
