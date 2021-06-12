2c441f48fd0faf2c646430f6c4bf84c16912c582
BBergsJ <goldlike888@gmail.com>
2021-06-12 23:32:20 +0300

    ForkJoinPool

--- src/main/java/ru/job4j/concurrent/fjp/ParallelSearcher.java
+++ src/main/java/ru/job4j/concurrent/fjp/ParallelSearcher.java
@@ -1,8 +1,11 @@
 package ru.job4j.concurrent.fjp;
 
+import ru.job4j.concurrent.buffer.ParallelSearch;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveTask;
-import java.util.stream.IntStream;
 
 public class ParallelSearcher extends RecursiveTask<Integer> {
     private final int[] array;
@@ -19,16 +22,43 @@ public class ParallelSearcher extends RecursiveTask<Integer> {
 
     @Override
     protected Integer compute() {
-
-        return -1;
+        int rsl = -1;
+        if (from == to) {
+            return -1;
+        } else if (array.length <= 10) {
+            rsl = serialSearch(array, index);
+        }
+        int mid = (from + to) / 2;
+        ParallelSearcher leftPS = new ParallelSearcher(array, from, mid, index);
+        ParallelSearcher rightPS = new ParallelSearcher(array, mid + 1, to, index);
+        leftPS.fork();
+        rightPS.fork();
+        Integer left = leftPS.join();
+        Integer right = rightPS.join();
+        return Math.max(left, right);
     }
 
     private int serialSearch(int[] array, int index) {
-        return IntStream.range(0, array.length - 1).filter(i -> array[i] == index).findFirst().orElse(-1);
+        for (int i = from; i < to; i++) {
+            if (array[i] == index) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     public static int find(int[] array, int index) {
         ForkJoinPool forkJoinPool = new ForkJoinPool();
         return forkJoinPool.invoke(new ParallelSearcher(array, 0, array.length - 1, index));
+    }
+
+    public static void main(String[] args) {
+        ParallelSearch parallelSearch = new ParallelSearch();
+        int index = 55;
+        int[] array = new int[100];
+        for (int i = 0; i < 100; i++) {
+            array[i] = i;
+        }
+        System.out.println(find(array, index));
     }
 }
