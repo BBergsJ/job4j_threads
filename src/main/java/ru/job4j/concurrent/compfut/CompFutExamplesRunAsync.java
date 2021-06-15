61524f376efdfa4f879f8c1df8c59a3e8d13adc4
BBergsJ <goldlike888@gmail.com>
2021-06-16 00:32:49 +0300

    CompletableFuture

--- src/main/java/ru/job4j/concurrent/compfut/CompFutExamplesRunAsync.java
+++ src/main/java/ru/job4j/concurrent/compfut/CompFutExamplesRunAsync.java
@@ -33,7 +33,26 @@ public class CompFutExamplesRunAsync {
         iWork();
     }
 
+    public static void thenRunExample() throws Exception {
+        CompletableFuture<Void> gtt = goToTrash();
+        gtt.thenRun(() -> {
+            int count = 0;
+            while (count < 3) {
+                System.out.println("Сын: я мою руки");
+                try {
+                    TimeUnit.MILLISECONDS.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                count++;
+            }
+            System.out.println("Сын: Я помыл руки");
+        });
+        iWork();
+    }
+
     public static void main(String[] args) throws Exception {
-        runAsyncExample();
+//        runAsyncExample();
+        thenRunExample();
     }
 }
