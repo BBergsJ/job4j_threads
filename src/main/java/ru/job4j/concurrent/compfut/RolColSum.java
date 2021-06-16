e1331d7ace12f3d6b92b7ced9aeb3d3c6ba76446
BBergsJ <goldlike888@gmail.com>
2021-06-16 23:35:12 +0300

    CompletableFuture

--- src/main/java/ru/job4j/concurrent/compfut/RolColSum.java
+++ src/main/java/ru/job4j/concurrent/compfut/RolColSum.java
@@ -5,6 +5,22 @@ public class RolColSum {
         private int rowSum;
         private int colSum;
         /* Getter and Setter */
+
+        public int getRowSum() {
+            return rowSum;
+        }
+
+        public void setRowSum(int rowSum) {
+            this.rowSum = rowSum;
+        }
+
+        public int getColSum() {
+            return colSum;
+        }
+
+        public void setColSum(int colSum) {
+            this.colSum = colSum;
+        }
     }
 
     public static Sums[] sum(int[][] matrix) {
